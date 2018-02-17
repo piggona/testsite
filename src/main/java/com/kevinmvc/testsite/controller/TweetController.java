fb0b1c817972bdf9f99a86ec83c91cf701c6d218
piggona <31783726+piggona@users.noreply.github.com>
2018-02-17 12:34:24 +0800

    Webjar
    Add Webjars to dispaly not only text,but also other information

--- src/main/java/com/kevinmvc/testsite/controller/TweetController.java
+++ src/main/java/com/kevinmvc/testsite/controller/TweetController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class TweetController {
@@ -20,11 +19,8 @@ public class TweetController {
     @RequestMapping("/twitter")
     public String hello(@RequestParam(defaultValue = "haohaojin's tweet") String search,Model model){
         SearchResults searchResults=twitter.searchOperations().search(search);//使用twitter对象进行搜索操作
-        List<String> tweets=searchResults.getTweets().//获取搜索到的结果（是一个集合）
-                stream().//集合默认包含的方法，为了对它进行函数式操作（操作可以是中间操作-返回一个流，也可以是终止操作-返回一个值）
-                map(Tweet::getText)./*中间操作有  map:为列表中每个元素都应用某个方法。filter:会返回匹配断言的所有元素。
-                                    reduce：借助一个操作和累加器将列表中的所有值聚合到单个值上*/
-                collect(Collectors.toList());//终止操作 Collectors.toList()说明会产生一个列表，包含流中每一个元素
+        List<Tweet> tweets=searchResults.getTweets();
+        model.addAttribute("search",search);
         model.addAttribute("tweets",tweets);
         return "tweet";
     }
