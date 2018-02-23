5b6ac046da597155d028fa9311c45f4411035d64
piggona <31783726+piggona@users.noreply.github.com>
2018-02-23 11:42:15 +0800

    i18n实现(初步）

--- src/main/java/com/kevinmvc/testsite/config/WebConfiguration.java
+++ src/main/java/com/kevinmvc/testsite/config/WebConfiguration.java
@@ -1,14 +1,35 @@
 package com.kevinmvc.testsite.config;
 
 import com.kevinmvc.testsite.date.USLocalDateFormatter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.time.LocalDate;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter{
+    @Bean
+    public LocaleResolver localeResolver(){//设置LocaleResolver的接口实现
+        return new SessionLocaleResolver();
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){//设置Locale信息的接收器
+        LocaleChangeInterceptor localeChangeInterceptor=new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
