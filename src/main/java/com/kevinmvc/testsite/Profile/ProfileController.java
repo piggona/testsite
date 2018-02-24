71bb04d93ef335c4e89d015eead8a9f5cc5fd4bc
piggona <31783726+piggona@users.noreply.github.com>
2018-02-24 14:42:29 +0800

    i18n

--- src/main/java/com/kevinmvc/testsite/Profile/ProfileController.java
+++ src/main/java/com/kevinmvc/testsite/Profile/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -16,7 +17,7 @@ public class ProfileController {
     public String displayProfile(ProfileForm profileForm){
         return "profile/profilePage";
     }
-    @RequestMapping(value="/profile",method= RequestMethod.POST)
+    @RequestMapping(value="/profile",params = {"save"}, method= RequestMethod.POST)
     public String saveProfile(@Valid ProfileForm profileForm,BindingResult bindingResult)
     {
         if(bindingResult.hasErrors())
@@ -26,6 +27,22 @@ public class ProfileController {
         System.out.println("save ok"+profileForm);
         return "redirect:/profile";
     }
+
+    @RequestMapping(value = "/profile",params = {"addTaste"})
+    public String addRow(ProfileForm profileForm)
+    {
+        profileForm.getTaste().add(null);
+        return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "profile",params = {"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest req)
+    {
+        Integer rowId=Integer.valueOf(req.getParameter("removeTaste"));
+        profileForm.getTaste().remove(rowId.intValue());
+        return "profile/profilePage";
+    }
+
 
     @ModelAttribute("dateFormat")//相当于model.addAttribute()将return的字符串加入"dateFormat"库
     public String localeFormat(Locale locale)
